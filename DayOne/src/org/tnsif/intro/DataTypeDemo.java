716143991968e54823514fe04157602945ef4a27
dasham <dashamrajbante07@gmail.com>
2023-02-02 13:20:21 +0530

    First Commit

---
+++ DayOne/src/org/tnsif/intro/DataTypeDemo.java
@@ -0,0 +1,48 @@
+package org.tnsif.intro;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		//byte=> 1byte
+		byte a=127;
+		
+		//integer=> 4byte
+		int b=128;
+		long b1=6778635344l;
+		
+		//float=>
+		
+		float c=12/5;
+		float d=5/2.5f;
+		
+		//double
+		double e=12/5;//2.0
+		double f=12d/5d;//2.4
+		
+		//character
+		char z='s';
+		
+		//ASCII Representation
+		int x='c';
+		
+		//string
+		String name="Dashamraj";
+		
+		//boolean type
+		boolean y=true;
+		
+		System.out.println("The value of a is: "+a);
+		System.out.println("The value of b is: "+b);
+		System.out.println("The value of b1 is: "+b1);
+		System.out.println("The value of c is: "+c);
+		System.out.println("The value of d is: "+d);
+		System.out.println("The value of e is: "+e);
+		System.out.println("The value of f is: "+f);
+		System.out.println("The value of z is: "+z);
+		System.out.println("The value of x is: "+x);
+		System.out.println("My name is: "+name);
+		System.out.println("The value of y is: "+y);
+
+	}
+
+}
