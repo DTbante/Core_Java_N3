abb3d491754834350b035d44b9bdb25bc260b63c
dasham <dashamrajbante07@gmail.com>
2023-02-21 00:53:37 +0530

    First Commit

---
+++ assignmenttwo/src/org/tnsif/stringdemo/ObjectClassDemo.java
@@ -0,0 +1,16 @@
+package org.tnsif.stringdemo;
+//program to demonstrate on Object class
+class Sample
+{
+	
+}
+public class ObjectClassDemo {
+
+	public static void main(String[] args) {
+		ObjectClassDemo s=new ObjectClassDemo();
+		System.out.println(s.getClass());
+		System.out.println(s.hashCode());
+
+	}
+
+}
