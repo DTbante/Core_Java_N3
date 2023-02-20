a5c5704dc1e15e5c10a57c4ff058cdaf9e8085c3
dasham <dashamrajbante07@gmail.com>
2023-02-21 00:16:00 +0530

    First Commit

---
+++ dayseven/src/org/tnsif/polymorphismdemo/MethodOverridingExecutor.java
@@ -0,0 +1,12 @@
+package org.tnsif.polymorphismdemo;
+//driver class
+public class MethodOverridingExecutor {
+
+	public static void main(String[] args) {
+		
+		SBI s = new SBI();
+		System.out.println(s.getInterest());
+
+	}
+
+}
---
+++ dayseven/src/org/tnsif/polymorphismdemo/RBI.java
@@ -0,0 +1,17 @@
+package org.tnsif.polymorphismdemo;
+//program to demonstrate on method overriding
+//parent class
+public class RBI {
+
+	float getInterest()
+	{
+		return 10.8f;
+	}
+	
+	//IF we use method overriding in same class we will get error,
+	// so we have to use inheritance to achieve method overriding
+//	float getInterest()
+//	{
+//		return 10.8f;
+//	}
+}
