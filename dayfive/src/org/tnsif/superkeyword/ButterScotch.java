9e95e644b7112a5e051b097ee98446077f1fdc9f
dasham <dashamrajbante07@gmail.com>
2023-02-20 23:42:25 +0530

    First Commit

---
+++ dayfive/src/org/tnsif/superkeyword/ButterScotch.java
@@ -0,0 +1,11 @@
+package org.tnsif.superkeyword;
+//child class
+public class ButterScotch extends Icecream{
+		
+	protected String flavour="Creamy ButterScotch";
+
+	public ButterScotch() {
+		//super();
+		System.out.println("Child class");
+	}
+}
